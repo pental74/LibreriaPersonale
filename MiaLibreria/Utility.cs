fe766fb80a6913a4a51323f7443738234d22726b
Marco Aquilanti <marco.aquilanti@iismarconipieralisi.it>
2025-01-31 19:36:49 +0100

    Primo aggiornamento

---
+++ MiaLibreria/Utility.cs
@@ -0,0 +1,68 @@
+﻿using System;
+// Libreria che contiene tutte funzioni e procedure che posso andare da utilizzare
+namespace MiaLibreria
+{
+    public class Utility
+    {
+        // Stampa di prova
+        public static void ProceduraDiProva (string Saluto)
+        {
+            Console.WriteLine (Saluto);
+        }
+
+        // Stampa di un vettore di stringhe
+        public static void StampaVettoreStringhe (string[] vettore_stringhe)
+        {
+            Console.WriteLine($"\n\nStampa vettore specifica per {nameof(vettore_stringhe)}\n");
+            for (int i = 0; i < vettore_stringhe.Length; i++)
+            {
+                // nameof(vettore_stringhe) = stampa il nome della variabile
+                Console.WriteLine($"{nameof(vettore_stringhe)}[{i}] = {vettore_stringhe[i]}");
+            }
+        }
+
+
+        // Stampa di un vettore di interi
+        public static void StampaVettoreInteri(int[] vettore_interi)
+        {
+            Console.WriteLine($"\n\nStampa vettore specifica per {nameof(vettore_interi)}\n");
+            for (int i = 0; i < vettore_interi.Length; i++)
+            {
+                // nameof(vettore_stringhe) = stampa il nome della variabile
+                Console.WriteLine($"{nameof(vettore_interi)}[{i}] = {vettore_interi[i]}");
+            }
+        }
+
+        #region Procedure di stampa generica utilizzabili con parametri di diverso tipo
+        public static void StampaVettore(int[] vettore_interi)
+        {
+            Console.WriteLine($"\n\nStampa vettore generica - {nameof(vettore_interi)}\n");
+            for (int i = 0; i < vettore_interi.Length; i++)
+            {
+                // nameof(vettore_stringhe) = stampa il nome della variabile
+                Console.WriteLine($"{nameof(vettore_interi)}[{i}] = {vettore_interi[i]}");
+            }
+        }
+
+        public static void StampaVettore(string[] vettore_stringhe)
+        {
+            Console.WriteLine($"\n\nStampa vettore generica - {nameof(vettore_stringhe)}\n");
+            for (int i = 0; i < vettore_stringhe.Length; i++)
+            {
+                // nameof(vettore_stringhe) = stampa il nome della variabile
+                Console.WriteLine($"{nameof(vettore_stringhe)}[{i}] = {vettore_stringhe[i]}");
+            }
+        }
+
+        public static void StampaVettore(char[] vettore_caratteri)
+        {
+            Console.WriteLine($"\n\nStampa vettore generica - {nameof(vettore_caratteri)}\n");
+            for (int i = 0; i < vettore_caratteri.Length; i++)
+            {
+                // nameof(vettore_stringhe) = stampa il nome della variabile
+                Console.WriteLine($"{nameof(vettore_caratteri)}[{i}] = {vettore_caratteri[i]}");
+            }
+        }
+        #endregion
+    }
+}
---
+++ Uso di libreria personale/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Using della libreria personale
+using MiaLibreria;
+
+namespace Uso_di_libreria_personale
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Vettore delle stringhe di prova
+            string[] strings = { "Prova", "Marco", "Ciao" };
+            int[] interi = { 1, 10, 2, 4, 6, 3, 7, 98, 9, 7, 5, 3 };
+            char[] caratteri = { 'a', 'b', '2', 'r', 'g', 'F', '7'};
+
+            // Richiamo della libreria personale, necessario inserire nome della classe 
+            Utility.ProceduraDiProva("Ciao Marco");
+
+            // Utilizzo di procedura di stampa specifiche per i diversi tipi di dati
+            Utility.StampaVettoreStringhe(strings);
+            Utility.StampaVettoreInteri(interi);
+
+            // Utilizzo della procedura di stampa con lo stesso nome con argomenti di diversi tipo
+            Utility.StampaVettore(interi);
+            Utility.StampaVettore(strings);
+            Utility.StampaVettore(caratteri);
+        }
+    }
+}
